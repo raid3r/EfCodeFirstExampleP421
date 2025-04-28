155a23745cd15f90442e19118cfd5f854ce81d03
Vasyl Kovalov <raid3r81@gmail.com>
2025-04-28 21:04:05 +0300

    Добавлен класс MyApp для работы с базой данных
    В этом коммите добавлен класс `MyApp`, который использует контекст базы данных `BookStoreContext`. Реализован метод `Run`, который извлекает список авторов и выводит их имена и фамилии в консоль.

---
+++ EfCodeFirstExample/MyApp.cs
@@ -0,0 +1,28 @@
+﻿using EfCodeFirstExample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EfCodeFirstExample;
+
+public class MyApp
+{
+    private readonly BookStoreContext _context;
+
+    public MyApp(BookStoreContext context)
+    {
+        _context = context;
+    }
+
+    public void Run()
+    {
+        // Example usage of the context
+        var authors = _context.Authors.ToList();
+        foreach (var author in authors)
+        {
+            Console.WriteLine($"{author.FirstName} {author.LastName}");
+        }
+    }
+}
